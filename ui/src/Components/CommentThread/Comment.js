7b8031d4c657b08cda6c7cd5abe45ba05bc4e612
Saif Chaudhry <sachaudh@users.noreply.github.com>
2020-01-08 14:14:47 -0800

    ROX-3820: Editing Violation Comments (#4336)

--- ui/src/Components/CommentThread/Comment.js
+++ ui/src/Components/CommentThread/Comment.js
@@ -1,38 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import PropTypes from 'prop-types';
 import { format } from 'date-fns';
-import { Edit, Trash2 } from 'react-feather';
+import { Edit, Trash2, XCircle } from 'react-feather';
 
 import dateTimeFormat from 'constants/dateTimeFormat';
 
-const Comment = ({ comment, onDelete }) => {
-    const { email, createdTime, updatedTime, message, canModify } = comment;
-    function onEdit() {
-        // do nothing for now
+import TextArea from 'Components/forms/TextArea';
+
+const ActionButtons = ({ isEditing, canModify, onToggleEdit, onDelete }) => {
+    if (isEditing) {
+        return (
+            <div>
+                <XCircle
+                    className="h-4 w-4 ml-2 text-success-800 cursor-pointer hover:text-success-500"
+                    onClick={onToggleEdit}
+                />
+            </div>
+        );
     }
+    return (
+        <div className={`${!canModify && 'invisible'}`}>
+            <Edit
+                className="h-4 w-4 mx-2 text-primary-800 cursor-pointer hover:text-primary-500"
+                onClick={onToggleEdit}
+            />
+            <Trash2
+                className="h-4 w-4 text-primary-800 cursor-pointer hover:text-primary-500"
+                onClick={onDelete}
+            />
+        </div>
+    );
+};
+
+const InputForm = ({ value, onSubmit }) => {
+    const { register, handleSubmit, errors } = useForm();
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <TextArea
+                name="message"
+                required
+                register={register}
+                errors={errors}
+                rows="5"
+                cols="33"
+                defaultValue={value}
+                placeholder="Write a comment here..."
+            />
+            <div className="flex justify-end">
+                <input
+                    className="bg-success-300 border border-success-800 p-1 rounded-sm text-sm text-success-900 uppercase hover:bg-success-400 cursor-pointer"
+                    type="submit"
+                    value="Save"
+                />
+            </div>
+        </form>
+    );
+};
+
+const Comment = ({ comment, onDelete, onSave, defaultEdit }) => {
+    const [isEditing, setEdit] = useState(defaultEdit);
+    const { email, createdTime, updatedTime, message, canModify } = comment;
+
+    const isCommentUpdated = updatedTime && createdTime !== updatedTime;
+
+    function onToggleEdit() {
+        setEdit(!isEditing);
+    }
+
+    function onSubmit(data) {
+        onToggleEdit();
+        onSave(comment, data.message);
+    }
+
     function onDeleteHandler() {
         onDelete(comment);
     }
-    const isCommentUpdated = updatedTime && createdTime !== updatedTime;
+
     return (
-        <div className="bg-primary-100 border border-primary-300 rounded-lg p-2">
+        <div
+            className={`${
+                isEditing
+                    ? 'bg-success-200 border-success-500'
+                    : 'bg-primary-100 border-primary-300'
+            } border rounded-lg p-2`}
+        >
             <div className="flex flex-1">
                 <div className="text-primary-800 flex flex-1">{email}</div>
-                <div className={`${!canModify && 'invisible'}`}>
-                    <Edit
-                        className="h-4 w-4 mx-2 text-primary-800 cursor-pointer hover:text-primary-500"
-                        onClick={onEdit}
-                    />
-                    <Trash2
-                        className="h-4 w-4 text-primary-800 cursor-pointer hover:text-primary-500"
-                        onClick={onDeleteHandler}
-                    />
-                </div>
+                <ActionButtons
+                    isEditing={isEditing}
+                    canModify={canModify}
+                    onToggleEdit={onToggleEdit}
+                    onDelete={onDeleteHandler}
+                />
             </div>
             <div className="text-base-500 text-xs mt-1">
                 {format(createdTime, dateTimeFormat)} {isCommentUpdated && '(edited)'}
             </div>
-            <div className="mt-2 text-primary-800 leading-normal">{message}</div>
+            <div className="mt-2 text-primary-800 leading-normal">
+                {isEditing ? <InputForm value={message} onSubmit={onSubmit} /> : message}
+            </div>
         </div>
     );
 };
@@ -46,7 +113,13 @@ Comment.propTypes = {
         updatedTime: PropTypes.string,
         canModify: PropTypes.bool
     }).isRequired,
-    onDelete: PropTypes.func.isRequired
+    onDelete: PropTypes.func.isRequired,
+    onSave: PropTypes.func.isRequired,
+    defaultEdit: PropTypes.bool
+};
+
+Comment.defaultProps = {
+    defaultEdit: false
 };
 
 export default Comment;
--- ui/src/Containers/Violations/SidePanel/ViolationComments.js
+++ ui/src/Containers/Violations/SidePanel/ViolationComments.js
@@ -1,45 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import CommentThread from 'Components/CommentThread';
 
-const ViolationComments = () => {
-    const comments = [
-        {
-            id: '1',
-            message:
-                'Completely unrelated, but check out this subreddit https://www.reddit.com/r/aww/',
-            email: 'sc@stackrox.com',
-            createdTime: '2019-12-29T21:21:31.218853651Z',
-            updatedTime: '2019-12-30T21:21:31.218853651Z',
-            canModify: true
-        },
-        {
-            id: '2',
-            message: 'Oh nice! This is the content I like',
-            email: 'ls@stackrox.com',
-            createdTime: '2019-12-30T21:21:31.218853651Z',
-            updatedTime: '2019-12-30T21:21:31.218853651Z',
-            canModify: false
-        },
-        {
-            id: '1',
-            message: 'Also, do you want to hear a joke?',
-            email: 'sc@stackrox.com',
-            createdTime: '2019-12-30T22:21:31.218853651Z',
-            updatedTime: '2019-12-30T22:21:31.218853651Z',
-            canModify: true
-        },
-        {
-            id: '2',
-            message: 'No',
-            email: 'ls@stackrox.com',
-            createdTime: '2019-12-31T21:21:31.218853651Z',
-            updatedTime: '2019-12-31T21:21:31.218853651Z',
-            canModify: false
-        }
-    ];
+const defaultComments = [
+    {
+        id: '1',
+        message: 'Completely unrelated, but check out this subreddit https://www.reddit.com/r/aww/',
+        email: 'sc@stackrox.com',
+        createdTime: '2019-12-29T21:21:31.218853651Z',
+        updatedTime: '2019-12-30T21:21:31.218853651Z',
+        canModify: true
+    },
+    {
+        id: '2',
+        message: 'Oh nice! This is the content I like',
+        email: 'ls@stackrox.com',
+        createdTime: '2019-12-30T21:21:31.218853651Z',
+        updatedTime: '2019-12-30T21:21:31.218853651Z',
+        canModify: false
+    },
+    {
+        id: '3',
+        message: 'Also, do you want to hear a joke?',
+        email: 'sc@stackrox.com',
+        createdTime: '2019-12-30T22:21:31.218853651Z',
+        updatedTime: '2019-12-30T22:21:31.218853651Z',
+        canModify: true
+    },
+    {
+        id: '4',
+        message: 'No',
+        email: 'ls@stackrox.com',
+        createdTime: '2019-12-31T21:21:31.218853651Z',
+        updatedTime: '2019-12-31T21:21:31.218853651Z',
+        canModify: false
+    }
+];
 
-    function onSave() {}
+const ViolationComments = () => {
+    const [comments, setComments] = useState(defaultComments);
+
+    function onSave(comment, message) {
+        const newComments = comments.filter(datum => datum.id !== comment.id);
+        newComments.push({ ...comment, message });
+        setComments(newComments);
+    }
 
     function onDelete() {}
 
