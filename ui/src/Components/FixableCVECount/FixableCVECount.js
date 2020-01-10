e857f6ba5a968051cbc06bd0d34de9a5fe54ea42
Van Wilson <wilson@stackrox.com>
2020-01-09 19:40:00 -0500

    ROX-3626: no-hover-color-change-on-CVE-pill-when-not-a-link (#4362)

--- ui/src/Components/FixableCVECount/FixableCVECount.js
+++ ui/src/Components/FixableCVECount/FixableCVECount.js
@@ -13,7 +13,9 @@ function stopPropagation(e) {
 }
 
 const CountElement = ({ count, url, fixable, hideLink, individualClasses }) => {
-    const classes = fixable ? 'text-success-800' : 'text-base-700 hover:text-primary-700';
+    const classes = fixable
+        ? 'text-success-800'
+        : `text-base-700 ${url && !hideLink ? 'hover:text-primary-700' : ''}`;
 
     // can't just pluralize() because of special requirements
     //   1 CVE or 2 CVEs
