890031e49fd1a47e750f9bd3f397bd9fe687ddcb
Rhama Sundara <49660438+rhamasv@users.noreply.github.com>
2020-02-14 13:36:40 -0800

    disable some more tests until dackbox enabled in master (#4685)

--- ui/cypress/integration/vulnmanagement/cvesListPages.test.js
+++ ui/cypress/integration/vulnmanagement/cvesListPages.test.js
@@ -3,7 +3,7 @@ import checkFeatureFlag from '../../helpers/features';
 import { url, selectors } from '../../constants/VulnManagementPage';
 import { hasExpectedHeaderColumns, allChecksForEntities } from '../../helpers/vmWorkflowUtils';
 
-describe('CVEs list Page and its entity detail page,sub list  validations ', () => {
+describe.skip('CVEs list Page and its entity detail page,sub list  validations ', () => {
     before(function beforeHook() {
         // skip the whole suite if vuln mgmt isn't enabled
         if (checkFeatureFlag('ROX_VULN_MGMT_UI', false)) {
