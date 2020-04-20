8d1adf90ffb98859437fad7e2f7e4620dfb8dae8
Connor Gorman <connorgorman@users.noreply.github.com>
2020-04-20 15:01:31 -0700

    Add new workload for longer running tests (#5425)

---
+++ sensor/kubernetes/fake/long_running_workload.go
@@ -0,0 +1,34 @@
+package fake
+
+import (
+	"time"
+
+	"github.com/stackrox/rox/pkg/kubernetes"
+)
+
+func init() {
+	workloadRegistry["long-running"] = longRunningWorkload
+}
+
+var (
+	longRunningWorkload = &workload{
+		DeploymentWorkload: []deploymentWorkload{
+			{
+				DeploymentType: kubernetes.Deployment,
+				NumDeployments: 200,
+				PodWorkload: podWorkload{
+					NumPods:           5,
+					NumContainers:     3,
+					LifecycleDuration: 2 * time.Minute,
+
+					ProcessWorkload: processWorkload{
+						ProcessInterval: 30 * time.Second, // deployments * pods / rate = process / second
+						AlertRate:       0.01,             // 5% of all processes will trigger a runtime alert
+					},
+				},
+				UpdateInterval:    100 * time.Second,
+				LifecycleDuration: 10 * time.Minute,
+			},
+		},
+	}
+)
