502bc6301195cce6d968b86083fb9bc17dc15598
Linda Song <ls@stackrox.com>
2020-05-14 16:36:54 -0700

    ROX-4900: Not displaying undefined values for whitelist scope in Policy Details (#5670)

--- ui/src/Containers/Policies/Wizard/Details/descriptors.js
+++ ui/src/Containers/Policies/Wizard/Details/descriptors.js
@@ -6,64 +6,12 @@ import {
     envVarSrcLabels,
 } from 'messages/common';
 
-const comparatorOp = {
-    GREATER_THAN: '>',
-    GREATER_THAN_OR_EQUALS: '>=',
-    EQUALS: '=',
-    LESS_THAN_OR_EQUALS: '<=',
-    LESS_THAN: '<',
-};
-
-const formatResourceValue = (prefix, value, suffix) =>
-    `${prefix} ${comparatorOp[value.op]} ${value.value} ${suffix}`;
-
-const formatResources = (resource) => {
-    const output = [];
-    if (resource.memoryResourceRequest) {
-        output.push(formatResourceValue('Memory request', resource.memoryResourceRequest, 'MB'));
-    }
-    if (resource.memoryResourceLimit) {
-        output.push(formatResourceValue('Memory limit', resource.memoryResourceLimit, 'MB'));
-    }
-    if (resource.cpuResourceRequest) {
-        output.push(formatResourceValue('CPU request', resource.cpuResourceRequest, 'Cores'));
-    }
-    if (resource.cpuResourceLimit) {
-        output.push(formatResourceValue('CPU limit', resource.cpuResourceLimit, 'Cores'));
-    }
-    return output.join(', ');
-};
-
-const formatScope = (scope, props) => {
-    if (!scope) return '';
-    const values = [];
-    if (scope.cluster !== '') {
-        let { cluster } = scope;
-        if (props.clustersById[scope.cluster]) {
-            cluster = props.clustersById[scope.cluster].name;
-        }
-        values.push(`Cluster:${cluster}`);
-    }
-    if (scope.namespace !== '') {
-        values.push(`Namespace:${scope.namespace}`);
-    }
-    if (scope.label) {
-        values.push(`Label:${scope.label.key}=${scope.label.value}`);
-    }
-    return values.join('; ');
-};
-
-const formatDeploymentWhitelistScope = (whitelistScope, props) => {
-    const values = [];
-    if (whitelistScope.name !== '') {
-        values.push(`Deployment Name:${whitelistScope.name}`);
-    }
-    const scopeVal = formatScope(whitelistScope.scope, props);
-    if (scopeVal !== '') {
-        values.push(scopeVal);
-    }
-    return values.join('; ');
-};
+import {
+    comparatorOp,
+    formatResources,
+    formatScope,
+    formatDeploymentWhitelistScope,
+} from './utils';
 
 // JSON value name mapped to formatting for description page.
 const fieldsMap = {
