e936a091ee328700c2a2f0cd8120c3e8e9c5ed67
lvalerom <lvalerom@redhat.com>
2023-06-20 15:56:58 +0200

    refactor resolver to use the uniqueue

--- sensor/kubernetes/eventpipeline/resolver/resolver.go
+++ sensor/kubernetes/eventpipeline/resolver/resolver.go
@@ -1,6 +1,8 @@
 package resolver
 
 import (
+	"github.com/stackrox/rox/pkg/concurrency"
+	"github.com/stackrox/rox/pkg/uniqueue"
 	"github.com/stackrox/rox/sensor/common/store"
 	"github.com/stackrox/rox/sensor/kubernetes/eventpipeline/component"
 )
@@ -8,8 +10,11 @@ import (
 // New instantiates a Resolver component
 func New(outputQueue component.OutputQueue, provider store.Provider, queueSize int) component.Resolver {
 	return &resolverImpl{
-		outputQueue:   outputQueue,
-		innerQueue:    make(chan *component.ResourceEvent, queueSize),
-		storeProvider: provider,
+		stopper:        concurrency.NewStopper(),
+		outputQueue:    outputQueue,
+		innerQueue:     nil,
+		innerQueueSize: queueSize,
+		storeProvider:  provider,
+		queue:          uniqueue.NewUniQueue[deploymentRef](queueSize),
 	}
 }
